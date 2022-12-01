af689fa033fc66841cbe8feafa5a8829b935a6a7
deshabml <deshab@yandex.ru>
2022-12-01 22:53:22 +0300

    Completed -remark2

--- Profile/ProfileHeaderView.swift
+++ Profile/ProfileHeaderView.swift
@@ -12,10 +12,7 @@ class ProfileHeaderView: UIView {
     private var statusText: String = "Waiting for something..."
 
     let nameCat: UILabel = {
-        let nameCat = UILabel(frame: CGRect(x: 0,
-                                            y: 0,
-                                            width: 100,
-                                            height: 30))
+        let nameCat = UILabel(frame: .zero)
         nameCat.text = "Hipster Cat"
         nameCat.font = UIFont.boldSystemFont(ofSize: 18)
         nameCat.textColor = .black
@@ -23,10 +20,7 @@ class ProfileHeaderView: UIView {
     }()
 
     let textStatus: UILabel = {
-        let textStatus = UILabel(frame: CGRect(x: 0,
-                                            y: 0,
-                                            width: 200,
-                                            height: 30))
+        let textStatus = UILabel(frame: .zero)
         textStatus.text = "Waiting for something..."
         textStatus.font = UIFont(name: "regular", size: 14)
         textStatus.textColor = .gray
@@ -37,10 +31,7 @@ class ProfileHeaderView: UIView {
 
 
     let button: UIButton = {
-        var button = UIButton(frame: CGRect(x: 0,
-                                            y: 0 + 95,
-                                            width: 50,
-                                            height: 50))
+        var button = UIButton(frame: .zero)
         button.setTitle("Show status", for: .normal)
         button.setTitleColor(.white, for: .normal)
         button.backgroundColor = .blue
@@ -54,24 +45,27 @@ class ProfileHeaderView: UIView {
 
     let avatar: UIView = {
 
-        var avatar = UIView(frame: CGRect(x: 16,
-                                          y: 16 + 95,
-                                          width: 100,
-                                          height: 100))
+        var avatar = UIView(frame: CGRect(x: 0,
+                                          y: 0,
+                                          width: 130,
+                                          height: 130))
         avatar.layer.cornerRadius = avatar.frame.size.width / 2
         avatar.clipsToBounds = true
         avatar.layer.borderColor = UIColor.white.cgColor
         avatar.layer.borderWidth = 3
         let myLayer = CALayer()
         let myImage = UIImage(named: "The_Cat")?.cgImage
-        myLayer.frame = CGRect(x: 0, y: 0, width: 100, height: 100)
+        myLayer.frame = CGRect(x: 0,
+                               y: 0,
+                               width: 130,
+                               height: 130)
         myLayer.contents = myImage
         avatar.layer.addSublayer(myLayer)
         return avatar
     }()
 
     let textFildOne: UITextField = {
-        let textFildOne = UITextField(frame: CGRect(x: 0, y: 0, width: 200, height: 40))
+        let textFildOne = UITextField(frame: .zero)
         textFildOne.backgroundColor = .white
         textFildOne.layer.cornerRadius = 12
         textFildOne.layer.borderWidth = 1
@@ -84,10 +78,7 @@ class ProfileHeaderView: UIView {
     }()
 
     init() {
-        super.init(frame: CGRect(x: 0,
-                                 y: 0,
-                                 width: 200,
-                                 height: 200))
+        super.init(frame: .zero)
     }
 
     @objc func buttonPressed() {
@@ -106,25 +97,25 @@ class ProfileHeaderView: UIView {
                                y: Int(frame.maxY) / 9 + 27,
                                width: 100,
                                height: 30)
-        nameCat.center.x = center.x
+        nameCat.center.x = center.x + 10
         addSubview(nameCat)
         textStatus.frame = CGRect(x: 0,
                                   y: Int(frame.maxY) / 9 + 84,
                                   width: 200,
                                   height: 30)
-        textStatus.center.x = center.x + 50
+        textStatus.center.x = center.x + 60
         addSubview(textStatus)
         textFildOne.frame = CGRect(x: 0,
                                    y: Int(frame.maxY) / 9 + 118,
                                    width: 200,
                                    height: 40)
-        textFildOne.center.x = center.x + 50
+        textFildOne.center.x = center.x + 60
         textFildOne.addTarget(self, action: #selector(statusTextChanged), for: .editingChanged)
         addSubview(textFildOne)
         avatar.frame = CGRect(x: 16,
                               y: Int(frame.maxY) / 9 + 16,
-                              width: 100,
-                              height: 100)
+                              width: 130,
+                              height: 130)
         addSubview(avatar)
         button.frame = CGRect(x: 0,
                               y: Int(frame.maxY) / 9 + 172,
