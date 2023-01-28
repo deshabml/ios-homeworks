fb59a5c8b272cbbaa91442871bec3b1987589844
deshabml <deshab@yandex.ru>
2023-01-28 17:09:34 +0300

    Added Animated photo

--- Navigation/Profile/Login/Profile/Photos/Controller/PhotosViewController.swift
+++ Navigation/Profile/Login/Profile/Photos/Controller/PhotosViewController.swift
@@ -11,6 +11,35 @@ class PhotosViewController: UIViewController {
 
     private var dataSource: [String] = Photos.shared.photos
 
+    private var isBigView: Bool = false
+
+    private lazy var backView: UIView = {
+        let backView = UIView()
+        backView.alpha = 0
+        backView.isHidden = true
+        backView.translatesAutoresizingMaskIntoConstraints = false
+        return backView
+    }()
+
+    private lazy var bigView: UIImageView = {
+        let bigView = UIImageView()
+        bigView.alpha = 0
+        bigView.isHidden = true
+        bigView.translatesAutoresizingMaskIntoConstraints = false
+        return bigView
+    }()
+
+    private lazy var exitImage: UIButton = {
+        let exitImage = UIButton()
+        exitImage.setImage(UIImage(systemName: "xmark"), for: .normal)
+        exitImage.addTarget(self,
+                            action: #selector(buttonExitPressed),
+                            for: .touchUpInside)
+        exitImage.isHidden = true
+        exitImage.translatesAutoresizingMaskIntoConstraints = false
+        return exitImage
+    }()
+
     private lazy var collectionView: UICollectionView = {
         let layout = UICollectionViewFlowLayout()
         layout.scrollDirection = .vertical
@@ -29,7 +58,12 @@ class PhotosViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .white
-        view.addSubview(collectionView)
+        view.addSubviews([
+            collectionView,
+            backView,
+            bigView,
+            exitImage
+        ])
         installingСonstraints()
     }
 
@@ -64,10 +98,54 @@ extension PhotosViewController {
             collectionView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             collectionView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             collectionView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            collectionView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            backView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            backView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            backView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            backView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            bigView.centerXAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerXAnchor),
+            bigView.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            bigView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            bigView.heightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor),
+            exitImage.topAnchor.constraint(equalTo: backView.topAnchor, constant: 16),
+            exitImage.trailingAnchor.constraint(equalTo: backView.trailingAnchor, constant: -16)
         ])
     }
-    
+
+    private func onTap() {
+        guard isBigView == false else { return }
+        isBigView = true
+        exitImage.alpha = 0
+        self.exitImage.isHidden = false
+        self.backView.isHidden = false
+        self.bigView.isHidden = false
+        UIView.animate(withDuration: 0.5, delay: 0, animations: {
+            self.backView.backgroundColor = .white
+            self.backView.alpha = 0.5
+        }) {_ in
+            UIView.animate(withDuration: 0.5, delay: 0, animations: {
+                self.bigView.alpha = 1
+            })
+        }
+        UIView.animate(withDuration: 0.3, delay: 0.5, animations: {
+            self.exitImage.alpha = 1
+        })
+
+    }
+
+    @objc func buttonExitPressed() {
+        UIView.animate(withDuration: 0.5, delay: 0, animations: {
+            self.bigView.alpha = 0
+            self.backView.alpha = 0
+        }) {_ in
+            self.backView.backgroundColor = UIColor(white: 1, alpha: 0)
+            self.backView.isHidden = true
+            self.bigView.isHidden = true
+            self.exitImage.isHidden = true
+        }
+        isBigView = false
+    }
+
 }
 
 extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
@@ -84,6 +162,13 @@ extension PhotosViewController: UICollectionViewDataSource, UICollectionViewDele
 
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         return CGSize(width: (UIScreen.main.bounds.width - 32) / 3, height: (UIScreen.main.bounds.width - 32) / 3)
+    }
+
+    func collectionView(_ collectionView: UICollectionView,
+                        didSelectItemAt indexPath: IndexPath) {
+        collectionView.deselectItem(at: indexPath, animated: true)
+        bigView.image = UIImage(named: dataSource[indexPath.item])
+        onTap()
     }
 
 }
--- Navigation/Profile/Login/Profile/Post/Controller/DetailPostViewController.swift
+++ Navigation/Profile/Login/Profile/Post/Controller/DetailPostViewController.swift
@@ -40,7 +40,7 @@ extension DetailPostViewController {
             tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
             tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
             tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
-            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
 
